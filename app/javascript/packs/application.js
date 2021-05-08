c0c89e57168e14d01039f18adf1323505c92c7b6
wkwkhautbois <kuwahautbois@gmail.com>
2021-05-09 00:31:50 +0900

    webpacker用のscssを作成、共通cssを記述 Sprocketsと共存しているので app/assets/stylesheets と app/javascript/stylesheets が共存する状態になる。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "bootstrap"
-import "bootstrap/scss/bootstrap.scss"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
